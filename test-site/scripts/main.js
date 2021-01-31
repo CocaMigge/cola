10637eae29cab83946f10eaece3f4053b147314f
Bayes <bayes.main@yahoo.com>
2021-01-31 22:28:47 +0800

    删除mozilla

---
+++ test-site/scripts/main.js
@@ -0,0 +1,36 @@
+/* 注：我们将 <script> 放在HTML文件的底部附近的原因是浏览器会按照代码在文件中的顺序加载 HTML。如果先
+加载的 JavaScript 期望修改其下方的 HTML，那么它可能由于 HTML 尚未被加载而失效。因此，
+将 JavaScript 代码放在 HTML页面的底部附近通常是最好的策略。*/
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/firefox-icon.png') {
+        myImage.setAttribute('Src', 'images/firefox2.png');
+    } else {
+        myImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+    let myName = prompt('请输入你的名字。');
+    if(!myName || myName === null) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Mozilla酷毙了, ' + myName;
+    }
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = '酷毙了， ' + storedName;
+}
+myButton.onclick = function(){
+    setUserName();
+}
---
+++ test-site/scripts/示例.js
@@ -0,0 +1,3 @@
+document.querySelector('htm1').onclick = function(){
+    alert('别戳我，我怕疼')
+}
